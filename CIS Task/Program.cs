ed9968b6647eed56496004e9767d814540f17dae
Mohamed Mohsen <moha476476@gmail.com>
2025-12-06 19:42:01 +0200

    Add project files.

---
+++ CIS Task/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace CIS_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountMelts("_I___I___I__I"));
+     
+        }
+
+        static int CountMelts(string s)
+        {
+            int total = 0;
+
+            while (true)
+            {
+                char[] arr = s.ToCharArray();
+                Stack<char> stack = new Stack<char>();
+                int meltIndex = -1;
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == '_')
+                    {
+                        stack.Push('_');
+
+                        // keep ONLY last 3 underscores
+                        if (stack.Count > 3)
+                            stack.Pop();
+
+                        if (stack.Count == 3)
+                        {
+                            int next = i + 1;
+                            if (next < arr.Length && arr[next] == 'I')
+                            {
+                                meltIndex = next;
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        stack.Clear();
+                    }
+                }
+
+                if (meltIndex == -1) break;
+
+                arr[meltIndex] = '_';
+                total++;
+                s = new string(arr);
+            }
+
+            return total;
+        }
+
+
+    }
+}
